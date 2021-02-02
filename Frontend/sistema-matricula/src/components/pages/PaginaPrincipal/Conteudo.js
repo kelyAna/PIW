4a37bc4c98164f97627e420c32d8506d5be6b658
kelyAna <kely@alu.ufc.br>
2021-02-02 22:04:59 +0000

    Classes dinâmicas

--- Frontend/sistema-matricula/src/components/pages/PaginaPrincipal/Conteudo.js
+++ Frontend/sistema-matricula/src/components/pages/PaginaPrincipal/Conteudo.js
@@ -1,8 +1,14 @@
-import './Conteudo.css'
+import './PaginaPrincipal.css'
 
-function Card({ nome, codigo }) {
+function Card({ nome, codigo, disponivel }) {
+
+    let nomeClasse = "card";
+
+    if (disponivel === false) {
+        nomeClasse += " fundo-vermelho";
+    }
     return (
-        <div className="card">
+        <div className={nomeClasse}>
             <h3 className="titulo-card">{nome}</h3>
             <span className="codigo-card">{codigo}</span>
         </div>
@@ -14,21 +20,30 @@ function Conteudo() {
         {
             nome: "LMS",
             codigo: "QXD253",
+            disponivel: false
         },
         {
             nome: "PIW",
             codigo: "QXD250",
+            disponivel: true
         },
         {
             nome: "PS",
             codigo: "QXD251",
+            disponivel: false
         }
 
     ]
-    let cards = disciplinas.map((disciplina)=>(
-                                <Card nome={disciplina.nome} codigo={disciplina.codigo}></Card>
-                                )
-                            )
+    let cards = disciplinas.map((disciplina) => (
+        <Card
+            nome={disciplina.nome}
+            codigo={disciplina.codigo}
+            disponivel={disciplina.disponivel}
+        >
+
+        </Card>
+    )
+    )
     return (
         <div className="conteudo-galeria">
             {cards}
