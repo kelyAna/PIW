08104f512b89498dcb499de4e567668fa8a11901
Ana Kely Lopes <kely@ufc.com.br>
2021-03-16 01:22:03 +0000

    React Hooks

--- Frontend/sistema-matricula/src/components/pages/PaginaPrincipal/PaginaPrincipal.js
+++ Frontend/sistema-matricula/src/components/pages/PaginaPrincipal/PaginaPrincipal.js
@@ -1,8 +1,22 @@
+import { useState } from 'react'
+
 import { Navegador } from "../../common/Navegador/Navegador";
 import Cabecalho from "./Cabecalho";
 import Conteudo from './Conteudo'
 
 import './PaginaPrincipal.css'
+
+function Botao(){
+    const [count, setCount] = useState(0)
+
+    function incrementarCount(){
+        setCount(count+1)
+    }
+
+    return <button onClick={incrementarCount}>
+        Eu fui pressionado {count} vezes
+    </button>
+}
 
 export function PaginaPrincipal() {
     return (
@@ -10,6 +24,7 @@ export function PaginaPrincipal() {
             <Cabecalho paginaAtual="pagina de matricula"></Cabecalho>
             <Navegador></Navegador>
             <Conteudo></Conteudo>
+            <Botao></Botao>
         </div>
     )
 }
